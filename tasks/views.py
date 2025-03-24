afd8c237a394e9831ffae30579c81ac968c8fb9f
Pedro Santana <pedrohsantana1@gmail.com>
2025-03-24 12:42:26 -0300

    Versão 6
    Create Dados

--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .models import Task
+from .forms import TaskForm
 
 def taskList(request):
-    tarefas = Task.objects.all()
+    tarefas = Task.objects.all().order_by('-created_at')
     return render(request, 'tasks/list.html', {'tarefas':tarefas})
 
 def taskView(request, id):
     tarefa = get_object_or_404(Task, pk=id)
     return render(request, 'tasks/task.html', {'tarefa':tarefa})
+
+def newTask(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.status = 'andamento'
+            task.save()
+            return redirect('/')
+
+    else:
+        form = TaskForm()
+        return render(request, 'tasks/addtask.html', {'form':form})
