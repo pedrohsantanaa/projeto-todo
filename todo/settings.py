4abbe70954d1c8dc958fde77eb98335cce8b91d5
Pedro Santana <97036217+pedrohsantanaa@users.noreply.github.com>
2025-04-23 18:37:49 -0300

    V11

--- todo/settings.py
+++ todo/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,9 +28,9 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
-ALLOWED_HOSTS = ['projeto-todo-fs60.onrender.com']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
@@ -87,14 +88,7 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'todo_db_h2f8',
-        'USER': 'admin',
-        'PASSWORD': 'sroo0giu1eBZxPO710yzuXLwZB4gZObI',
-        'HOST': 'dpg-d04kges9c44c739o46o0-a.oregon-postgres.render.com',
-        'PORT': '5432'
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600), 
 }
 
 
